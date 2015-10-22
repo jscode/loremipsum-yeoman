0c0c5a17a4ff752ce2bfdf439a2fc0915515848d
Jeffrey L Sullivan Jr <jeff@sullivanjr.org>
2015-10-22 13:09:06 -0700

    Initial commit with starting layout

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loremipsumApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the loremipsumApp
+ */
+angular.module('loremipsumApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loremipsumApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the loremipsumApp
+ */
+angular.module('loremipsumApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
